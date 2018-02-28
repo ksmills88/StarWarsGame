4b15a45f3734f92573dbbe543785e826acba0c8d
kmills <ksmills82@gmail.com>
2018-02-28 00:04:28 -0500

    created object in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+
+// var characters = [];
+// var obi = {
+//     "name": "Obi-Wan Kenobi",
+//     "healthPoint": 120,
+//     "attackPower": 8,
+//     "counterAttackPower": 12
+//   };
+// var luke = {
+//     "name": "Luke Skywalker",
+//     "healthPoint": 180,
+//     "attackPower": 8,
+//     "counterAttackPower": 12
+//   };
+// var maul = {
+//     "name": "Darth Maul",
+//     "healthPoint": 120,
+//     "attackPower": 8,
+//     "counterAttackPower": 12
+//   };
+// var sidious = {
+//     "name": "Darth Sidious",
+//     "healthPoint": 120,
+//     "attackPower": 8,
+//     "counterAttackPower": 12
+//   };
+// characters.push(obi, maul, luke, sidious);
+var isCharChosen = false;
+var attacker;
+var defender;
+var combatants = [];
+var characters = {
+    "obi": {
+        "name": "Obi-Wan Kenobi",
+        "healthPoint": 120,
+        "attackPower": 8,
+        "counterAttackPower": 12
+    },
+    "luke": {
+        "name": "Luke Skywalker",
+        "healthPoint": 180,
+        "attackPower": 25,
+        "counterAttackPower": 10
+    },
+    "maul": {
+        "name": "Darth Maul",
+        "healthPoint": 125,
+        "attackPower": 5,
+        "counterAttackPower": 50
+    },
+    "sidious": {
+        "name": "Darth Sidious",
+        "healthPoint": 200,
+        "attackPower": 10,
+        "counterAttackPower": 10
+    }
+}
+
+for(var i=0; i<characters.length)
+ 
+
+console.log(characters)
+  
+// $("#start-character").on("click", function (){
+//   $("#start-character").empty();
+// });
+
